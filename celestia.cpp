5395037114506b990d6c71b803517ef2a71148e0
Thibault Dupuis <thib.dupuis@gmail.com>
2017-03-30 12:20:37 +0200

    Add basic constants

--- celestia.cpp
+++ celestia.cpp
@@ -21,3 +21,32 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
 */
+#include "celestia.h"
+
+namespace {
+
+// Mathematical constants
+constexpr double kPI = 3.14159265358979323846;
+constexpr double kEulerNumber = 2.71828182845904523536;
+
+// Physical constants
+constexpr double kGravitation = 6.67259e-11;
+constexpr double kGravityAcceleration = 9.80665;
+
+constexpr unsigned int kSpeedLight = 299792458;
+constexpr unsigned int kStandardAtmosphere = 101325;
+
+// Astronomical constants
+constexpr unsigned int kAstronomicalUnit = 149597870;
+constexpr double kLightYear = 9.460530e12;
+
+constexpr unsigned int kRadiusSun = 696000;
+constexpr double kMassSun = 1.9891e30;
+
+constexpr double kRadiusEarth = 6378.137;
+constexpr double kMassEarth = 5.9737e24;
+
+// Spaceflight constants
+constexpr double kGravitationalEarth = 3.986004418e14;
+
+}
